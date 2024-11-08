1a2ac24c0ef3d168e0e140ba4ba3cbab8e3a496f
ZprKris <zaporozhetskristina@gmail.com>
2024-11-08 15:17:03 -0500

    made some edits to comments

--- financialProductsPrj/SharedPtr.h
+++ financialProductsPrj/SharedPtr.h
@@ -11,7 +11,7 @@ A smart pointer is a composition class that is designed to manage dynamically al
 that memory gets deleted when the smart pointer object goes out of scope.
 
 There are different types of smart pointers: std::unique_ptr and std::shared_ptr.
-In this code, std::shared_ptr is implemented.
+In this project, std::shared_ptr is implemented and std::unique_ptr is used directly.
 */
 
 /*
@@ -22,11 +22,12 @@ meant to solve the case where you need multiple smart pointers co-owning a resou
 Internally, std::shared_ptr keeps track of how many std::shared_ptr are sharing the resource.
 As long as at least one std::shared_ptr is pointing to the resource,
 the resource will not be deallocated, even if individual std::shared_ptr are destroyed.
+
 std::shared_ptr instances must know about each other!
 */
 
 /*
-MOVE SEMANTICS
+MOVE SEMANTICS:
 
 We use move semantics to prevent copying the objects in unnecessary cases, when objects
 are stored in temporary objects and will not be used later in the program.
@@ -34,24 +35,32 @@ Move copy constructor and assignment take in rvalue references (however, automat
 from a function by value can be moved even if they are l-values, because they are temporary).
 */
 
+/*
+TEMPLATE:
+
+Both the declaration and implementation of the SharedPtr template class are provided in the same header file.
+This is because compiler needs access to the template definition at the point of instantiation. 
+This ensures that the compiler can generate both the declaration and definition versions of the 
+template for a specific type. 
+*/
 
 #ifndef SHAREDPTR_H
 #define SHAREDPTR_H
 
 #include <iostream>
 
-
 template <typename T>
-class SharedPtr {
+class SharedPtr 
+{
 private:
     // Raw pointer
     T* m_ptr;
-    // Count of instances 
+    // Count of instances
     size_t* m_cnt;
 
 public:
     // Constructor
-    SharedPtr(T* p = nullptr);
+    explicit SharedPtr(T* p = nullptr);
     // Copy Constructor
     SharedPtr(const SharedPtr<T>& other);
     // Move Constructor
@@ -64,7 +73,6 @@ public:
     SharedPtr& operator=(SharedPtr&& other) noexcept;
 
     T& operator*() const;
-    // An interface to treat SharedPtr instances as pointers
     T* operator->() const;
     T* get_ptr() const;
     size_t get_cnt() const;
@@ -72,20 +80,23 @@ public:
 
 // Constructor
 template <typename T>
-SharedPtr<T>::SharedPtr(T* p) : m_ptr(p), m_cnt(new size_t(1)) {
+SharedPtr<T>::SharedPtr(T* p) : m_ptr(p), m_cnt(new size_t(1)) 
+{
     std::cout << "Shared pointer created. Count: " << *m_cnt << std::endl;
 }
 
 // Copy constructor
 template <typename T>
-SharedPtr<T>::SharedPtr(const SharedPtr<T>& other) : m_ptr(other.m_ptr), m_cnt(other.m_cnt) {
+SharedPtr<T>::SharedPtr(const SharedPtr<T>& other) : m_ptr(other.m_ptr), m_cnt(other.m_cnt) 
+{
     ++(*m_cnt);
     std::cout << "Shared pointer copied. Count: " << *m_cnt << std::endl;
 }
 
 // Move constructor
 template <typename T>
-SharedPtr<T>::SharedPtr(SharedPtr<T>&& other) noexcept : m_ptr(other.m_ptr), m_cnt(other.m_cnt) {
+SharedPtr<T>::SharedPtr(SharedPtr<T>&& other) noexcept : m_ptr(other.m_ptr), m_cnt(other.m_cnt) 
+{
     other.m_ptr = nullptr;
     other.m_cnt = nullptr;
     std::cout << "Shared pointer moved." << std::endl;
@@ -93,7 +104,8 @@ SharedPtr<T>::SharedPtr(SharedPtr<T>&& other) noexcept : m_ptr(other.m_ptr), m_c
 
 // Destructor
 template <typename T>
-SharedPtr<T>::~SharedPtr() {
+SharedPtr<T>::~SharedPtr() 
+{
     if (m_cnt && --(*m_cnt) == 0) {
         delete m_ptr;
         delete m_cnt;
@@ -103,7 +115,8 @@ SharedPtr<T>::~SharedPtr() {
 
 // Copy assignment operator
 template <typename T>
-SharedPtr<T>& SharedPtr<T>::operator=(const SharedPtr<T>& other) {
+SharedPtr<T>& SharedPtr<T>::operator=(const SharedPtr<T>& other) 
+{
     if (this != &other) {
         if (m_cnt && --(*m_cnt) == 0) {
             delete m_ptr;
@@ -120,7 +133,8 @@ SharedPtr<T>& SharedPtr<T>::operator=(const SharedPtr<T>& other) {
 
 // Move assignment operator
 template <typename T>
-SharedPtr<T>& SharedPtr<T>::operator=(SharedPtr<T>&& other) noexcept {
+SharedPtr<T>& SharedPtr<T>::operator=(SharedPtr<T>&& other) noexcept 
+{
     if (this != &other) {
         if (m_cnt && --(*m_cnt) == 0) {
             delete m_ptr;
@@ -137,22 +151,26 @@ SharedPtr<T>& SharedPtr<T>::operator=(SharedPtr<T>&& other) noexcept {
 }
 
 template <typename T>
-T& SharedPtr<T>::operator*() const {
+T& SharedPtr<T>::operator*() const 
+{
     return *m_ptr;
 }
 
 template <typename T>
-T* SharedPtr<T>::operator->() const {
+T* SharedPtr<T>::operator->() const 
+{
     return m_ptr;
 }
 
 template <typename T>
-T* SharedPtr<T>::get_ptr() const {
+T* SharedPtr<T>::get_ptr() const 
+{
     return m_ptr;
 }
 
 template <typename T>
-size_t SharedPtr<T>::get_cnt() const {
+size_t SharedPtr<T>::get_cnt() const 
+{
     return m_cnt ? *m_cnt : 0;
 }
 
--- financialProductsPrj/main.cpp
+++ financialProductsPrj/main.cpp
@@ -5,25 +5,41 @@
 #include "FinancialProducts.h"
 #include "SharedPtr.h"
 
-void displayFinancialProduct(const std::unique_ptr<FinancialProduct>& product) {
-    std::cout << product->getDescription() << " | Total Value: $" << std::fixed << std::setprecision(2) << product->getValue() << std::endl << std::endl;
+void displayFinancialProduct(const std::unique_ptr<FinancialProduct>& product) 
+{
+    std::cout << product->getDescription()
+        << " | Total Value: $"
+        << std::fixed << std::setprecision(2)
+        << product->getValue()
+        << std::endl << std::endl;
 }
 
-int main() {
+int main() 
+{
+    // Vector to store inventory of financial products
     std::vector<std::unique_ptr<FinancialProduct>> inventory;
 
+    // Adding products to inventory using unique_ptr
     inventory.push_back(std::make_unique<Stock>("Tech Company", 150.00, 20));
     inventory.push_back(std::make_unique<Bond>("Government Bond", 1000.00, 0.05));
     inventory.push_back(std::make_unique<Portfolio>("Balanced Portfolio", 150.00, 10, 5000.00, 0.03));
 
     std::cout << "Financial Product Inventory:\n\n";
-    for (const auto& product : inventory) {
+
+    // Displaying details of each financial product in the inventory
+    for (const auto& product : inventory) 
+    {
+        // Each product is treated generically as a FinancialProduct, but thanks to polymorphism, 
+        // each type (Stock, Bond, Portfolio) displays unique behavior in getValue() and getDescription() virtual functions 
         displayFinancialProduct(product);
     }
 
+    // Using user-defined SharedPtr class to manage a Stock instance 
     SharedPtr<FinancialProduct> ptr1(new Stock("Tech Company", 150.00, 20));
-    SharedPtr<FinancialProduct> ptr2 = ptr1;
-
+    // Copying the shared pointer, because shared pointers should be aware of each other 
+    // Count attribute should be incremented
+    SharedPtr<FinancialProduct> ptr2 = ptr1;  
 
     return 0;
 }
+
